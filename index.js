5f8771d7209add8b816bd2feafec640e9c78df75
Mayukh <mayukhchatterjee2023@gmail.com>
2023-08-18 10:59:15 +0530

    :roups added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 const User = require("./models/user");
 const Chat = require("./models/chat");
 const authController = require("./controller/authcontroller");
+const Groups = require("./models/groups");
+const Usergroups = require("./models/usergroup");
 
 const app = express();
 app.use(cors());
@@ -12,11 +14,11 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   const token = req.headers.authorization;
-  console.log(token);
+  // console.log(token);
   if (token) {
     const decodedToken = jwt.verify(token, "abcdxyztrsdgpjslyytfdcbf");
     const userId = decodedToken.userId;
-    console.log("USERID", userId);
+    // console.log("USERID", userId);
     User.findByPk(userId)
       .then((user) => {
         req.user = user;
@@ -43,7 +45,7 @@ app.post("/chats", async (req, res) => {
     const { message } = req.body;
     console.log(req.user.dataValues.id);
     const newChat = await Chat.create({
-      userId:req.user.dataValues.id,
+      userId: req.user.dataValues.id,
       message: message,
     });
 
@@ -55,20 +57,72 @@ app.post("/chats", async (req, res) => {
 });
 
 app.get("/getAllMessage", async (req, res) => {
-  console.log(req.user)
+  console.log(req.user);
   try {
     const message = await Chat.findAll({
-      attributes: ["message", "userId","id"],
-      include:[{model:User, attributes:["name"]}]
+      attributes: ["message", "userId", "id"],
+      include: [{ model: User, attributes: ["name"] }],
     });
-    res.status(201).json({data:message})
+    res.status(201).json({ data: message });
   } catch (err) {
-    res.status(500).json({error:err})
+    res.status(500).json({ error: err });
   }
 });
 
+app.post("/creategroup", async (req, res) => {
+  try {
+    const { groupname } = req.body;
+    const userId = req.user.dataValues.id; 
+    const phonenumber = req.user.phonenumber;
+
+    const group = await Groups.create({
+      groupname: groupname,
+      phonenumber: phonenumber,
+    });
+
+    // Associate the user with the created group
+    await group.setUser(userId);
+
+    res.status(201).json({ data: group });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+app.get("/admingroups", async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Invalid User" });
+    }
+
+    const userId = req.user.dataValues.id;
+
+    // Retrieve group names where userId matches
+    const groups = await Groups.findAll({
+      attributes: ["groupname","id"],
+      where: {
+        userId: userId,
+      },
+    });
+
+    res.status(200).json({ data: groups });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+
+
 User.hasMany(Chat);
 Chat.belongsTo(User);
+User.hasMany(Groups);
+Groups.belongsTo(User);
+Groups.hasMany(Chat);
+Chat.belongsTo(Groups);
+User.hasMany(Usergroups);
+Groups.hasMany(Usergroups);
+Usergroups.belongsTo(User);
+Usergroups.belongsTo(Groups);
 
 sequelize
   .sync()
